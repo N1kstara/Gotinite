60771b56b0e61f4a1348ea9a13629b919e7e0bbb
N1kstara <159249045+N1kstara@users.noreply.github.com>
2024-02-07 12:40:37 +0200

    Add files via upload

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,32 @@
+﻿int days = int.Parse(Console.ReadLine());
+int treated = 0;
+int untreated = 0;
+int doctors = 7;
+if(days>0 && days < 1001)
+{
+    for (int i = 0; i < days; i++)
+    {
+        int patients = int.Parse(Console.ReadLine());
+        if (patients < 10000)
+        {
+            if(patients < 8)
+            {
+                treated = treated + patients;
+            }
+            if (patients > 7 && (i+2) % 3 != 0)
+            {
+                treated += doctors;
+                untreated += (patients - doctors);
+            }
+            if (patients > 7 && (i+2) % 3 == 0)
+            {
+                treated += doctors;
+                untreated += (patients - doctors);
+                doctors++;
+            }
+
+        }
+    }
+}
+Console.WriteLine("Treated patients: " + treated);
+Console.WriteLine("Untreated patients: " + untreated);
